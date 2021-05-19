0d2dbb31a481cb7b9be41d360f96aeb5af055145
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-19 14:00:47 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 19/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swiftui_unit_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_unit_converterApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_unit_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
