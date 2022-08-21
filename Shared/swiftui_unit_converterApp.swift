4e59f0c1e89f4f356722056935900aacc56d8ca9
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-08-22 00:35:12 +0530

    Added macOS Support

--- Shared/swiftui_unit_converterApp.swift
+++ Shared/swiftui_unit_converterApp.swift
@@ -11,11 +11,15 @@ import SwiftUI
 struct swiftui_unit_converterApp: App {
     var body: some Scene {
         WindowGroup {
+            #if os(macOS)
+            SidebarPresenterView()
+            #else
             if UIDevice.current.userInterfaceIdiom == .phone {
                 TabbedView()
             } else {
                 SidebarPresenterView()
             }
+            #endif
         }
     }
 }
